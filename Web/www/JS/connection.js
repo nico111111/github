bc6a810660a7113c84a8f4c140d0fd8ead74a1a2
TantePanique <maxime.doubre@gmail.com>
2019-04-29 10:29:42 +0200

    création fichiers de connections

---
+++ Web/www/JS/connection.js
@@ -0,0 +1,29 @@
+var form = document.getElementById("connect");
+
+document.getElementById("username").addEventListener("blur", function (e) {
+  var text="";
+  var regex= /.{6,}/;
+  if (!regex.test(e.target.value)) {
+      text="Nom d'tilisateur invalide";
+      e.target.style.backgroundColor="#E45A5A";
+  }
+  if (regex.test(e.target.value)) {
+    e.target.style.backgroundColor="#FFFFFF";
+  }
+  console.log("coucou" + text);
+  document.getElementById("usernameErr").innerHTML = text;
+});
+
+document.getElementById("password").addEventListener("blur", function (e) {
+  var text="";
+  var regex= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+  if (!regex.test(e.target.value)) {
+      text="Mot de passe invalide";
+      e.target.style.backgroundColor="#E45A5A"; 
+  }
+  if (regex.test(e.target.value)) {
+    e.target.style.backgroundColor="#FFFFFF";
+  }
+  document.getElementById("passwordErr").innerHTML = text;
+  
+});
